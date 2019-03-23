e878e6a48ce8127cd6efc7f7a58c1f13918b6751
valentin <vali.voilean@gmail.com>
2019-03-23 11:46:57 +0100

    test header

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,3 +1,4 @@
+import { shallow } from 'enzyme';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
@@ -6,6 +7,8 @@ import 'jest-styled-components';
 import { Header } from './Header';
 
 import { StoreStyledProvider } from '../shared/StoreStyledProvider';
+
+import { firstWaypoint, secondWaypont } from './constants';
 
 describe('Header', () => {
   it('renders without crashing', () => {
@@ -17,5 +20,35 @@ describe('Header', () => {
       )
       .toJSON();
     expect(header).toMatchSnapshot();
+  });
+
+  describe('checkTopOffset method', () => {
+    const wrapper: any = shallow(<Header />);
+
+    it('should be displayed the entire header initially', () => {
+      expect(wrapper.state()).toEqual({
+        isNavigationVisible: true,
+        isHeaderVisible: true
+      });
+    });
+
+    it(`it should hide the menu when the user scrolls more than ${firstWaypoint}`, () => {
+      (global as any).scrollY = firstWaypoint + 1;
+      wrapper.instance().checkTopOffset();
+
+      expect(wrapper.state()).toEqual({
+        isNavigationVisible: false,
+        isHeaderVisible: true
+      });
+    });
+
+    it(`it should hide the header when the user scrolls more than ${secondWaypont}`, () => {
+      (global as any).scrollY = secondWaypont + 1;
+      wrapper.instance().checkTopOffset();
+      expect(wrapper.state()).toEqual({
+        isNavigationVisible: false,
+        isHeaderVisible: false
+      });
+    });
   });
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,11 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import throttle from 'lodash.throttle';
 
 configure({ adapter: new Adapter() });
+
+// mock the throttle method to bypass the timeout
+jest.mock('lodash.throttle');
+(throttle as any).mockImplementation((fn: any) => fn);
 
 export default undefined;
