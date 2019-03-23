d0c4a7ebe502e68c7ca31c1489a64cdc95e62a19
valentin <vali.voilean@gmail.com>
2019-03-23 09:23:36 +0100

    install enzyme; update app test

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,9 +1,23 @@
+import { shallow } from 'enzyme';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { App } from './App';
+
+import { defaultTheme } from '../store/theme/constants';
+
+describe('App', () => {
+  const props = {
+    theme: defaultTheme,
+    loadTheme: jest.fn()
+  };
+  let wrapper;
+
+  it('renders without crashing', () => {
+    shallow(<App {...props} />);
+  });
+
+  it('loads the theme on componentDidMount', () => {
+    wrapper = shallow(<App {...props} />);
+    expect(props.loadTheme).toHaveBeenCalled();
+  });
 });
